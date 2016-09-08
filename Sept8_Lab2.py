e39a1bb50ec26cd89cf7550195430bf75a4b63d3
Cassie Hudson <cassie.hudson@unt.edu>
2016-09-08 10:46:51 -0500

    Addition, subtraction, multiplication and division of two variables using if-elif-else commands--not working properly. Not sure how to define condition for if operation =

---
+++ Sept8_Lab2.py
@@ -0,0 +1,11 @@
+n1 = float (input("Enter number 1: ")); #Float does typecasting (nested statement)
+n2 = float (input("Enter number 2: "));
+operation = raw_input("Enter operation: ");
+if (operation = add):
+    print "Result of sum is: " + str(n1+n2); #Must convert to string to in order to be used with the print statement
+elif (operation multiply):
+    print "Result of product is:" + str(n1*n2);
+elif (operation == subtract):
+    print "Result of subtraction is: ", (n1-n2); #Use , to replace addition sign
+elif (operation == divide):
+    print "Result of division is: ", (n1/n2);
