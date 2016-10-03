d9c3ea2b908ee710aee8f9dc89ba1bb90bce0fea
Cassie Hudso <cassie.hudson@unt.edu>
2016-10-03 15:06:51 -0500

    Chapter 3 Decision Structures Programming Exercises

---
+++ Oct3.Ch3ProgrammingExercises.py
@@ -0,0 +1,24 @@
+#2. Areas of Rectangles
+lw1 = input("Please enter the length and width of the first rectangle, separated by a comma: ")
+lw2 = input("Please enter the length and width of the second rectangle, separated by a comma: ")
+
+
+
+#1. Day of the Week
+weekDay = input("Please choose a number between 1 and 7: ")
+if weekDay == 1:
+    print "Monday"
+elif weekDay == 2:
+    print "Tuesday"
+elif weekDay == 3:
+    print "Wednesday"
+elif weekDay == 4:
+    print "Thursday"
+elif weekDay == 5:
+    print "Friday"
+elif weekDay == 6:
+    print "Saturday"
+elif weekDay == 7:
+    print "Sunday"
+else:
+    print "Error!"
